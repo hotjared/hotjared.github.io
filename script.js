37070d25e8d54b4d6be16803710fd939a6567f75
Jared Rodriguez <jared7628@gmail.com>
2022-09-25 02:43:30 -0400

    Add listener to change grid spaces to black upon hover

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 const gridContainer = document.querySelector("#grid-container");
 
 //Set grid size
-let gridSize = 8;
-let spaceSize = 500 / gridSize;
+let gridSize = 16;
+let spaceSize = gridContainer.clientWidth / gridSize;
+
 
 //Create Grid Spaces
 for (let i = 0; i < gridSize ** 2; i++) {
-    let gridSpace = document.createElement('div');
+    const gridSpace = document.createElement('div');
     gridSpace.setAttribute('class', 'grid-space');
     gridSpace.setAttribute('style', `width: ${spaceSize}px; height: ${spaceSize}px`);
+    //Add listener to change gridSpace background to black when hovering over
+    gridSpace.addEventListener('mouseenter', () => {
+        gridSpace.style.backgroundColor = "black";
+    });
     gridContainer.appendChild(gridSpace);
+
 };
+
+const gridSpace = document.querySelector(".grid-space");
+gridSpace.addEventListener('mouseenter', () => {
+    gridSpace.style.backgroundColor = "black";
+});
+
 
